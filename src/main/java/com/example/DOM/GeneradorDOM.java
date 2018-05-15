21a3e7a61980bd0385a240a8aae7f9660fafe12b
Deyviz <Deyviz@192.168.1.38>
2018-05-15 03:10:39 -0500

    sa

--- src/main/java/com/example/DOM/GeneradorDOM.java
+++ src/main/java/com/example/DOM/GeneradorDOM.java
@@ -11,12 +11,12 @@ import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Result;
 import javax.xml.transform.Source;
 import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
 
+import org.apache.xml.security.utils.Constants;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
@@ -27,6 +27,7 @@ public class GeneradorDOM {
 	public GeneradorDOM() throws ParserConfigurationException {
 
 		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setNamespaceAware(true);
 		DocumentBuilder builder = factory.newDocumentBuilder();
 		document = builder.newDocument();
 		
@@ -34,28 +35,57 @@ public class GeneradorDOM {
 	
 	public void generarDocument() {
 		
-		Element productos = document.createElement("productos");
-		document.appendChild(productos);
-
-		
-		Element producto = document.createElement("producto");
-		productos.appendChild(producto);
-		producto.setAttribute("codigo", "1");
-		
-		Element nombre = document.createElement("nombre");
-		Element nombre2= document.createElement("nombre");
-		nombre.appendChild(document.createTextNode("Teclado"));
-		nombre2.appendChild(document.createTextNode("Monitor"));		
-		producto.appendChild(nombre);
-		producto.appendChild(nombre2);
-		
-		Element producto2 = document.createElement("producto");
-		productos.appendChild(producto2);
-		producto2.setAttribute("codigo", "2");
-		
-		Element nombre3 = document.createElement("nombre");
-		nombre3.appendChild(document.createTextNode("nombre"));
-		producto2.appendChild(nombre3);
+		 Element elemento = document.createElementNS("", "Invoice");
+		 elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns", "urn:oasis:names:specification:ubl:schema:xsd:Invoice-2");
+		 elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:cac", "urn:oasis:names:specification:ubl:schema:xsd:CommonAggregateComponents-2");
+		 elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:cbc", "urn:oasis:names:specification:ubl:schema:xsd:CommonBasicComponents-2");
+		 elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:ccts", "urn:un:unece:uncefact:documentation:2");
+		 elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:ds", "http://www.w3.org/2000/09/xmldsig#");
+         elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:ext", "urn:oasis:names:specification:ubl:schema:xsd:CommonExtensionComponents-2");
+         elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:qdt", "urn:oasis:names:specification:ubl:schema:xsd:QualifiedDatatypes-2");
+         elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:sac", "urn:sunat:names:specification:ubl:peru:schema:xsd:SunatAggregateComponents-1");
+         elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:udt", "urn:un:unece:uncefact:data:specification:UnqualifiedDataTypesSchemaModule:2");
+         elemento.setAttributeNS(Constants.NamespaceSpecNS, "xmlns:xsi", "http://www.w3.org/2001/XMLSchema-instance");
+         elemento.appendChild(document.createTextNode("\n"));
+         document.appendChild(elemento);
+         
+         Element UBLExtensions = document.createElementNS("", "ext:UBLExtensions");
+         elemento.appendChild(UBLExtensions);
+         Element UBLExtensions2 = document.createElementNS("", "ext:UBLExtension");
+         UBLExtensions2.appendChild(document.createTextNode("\n"));
+         Element ExtensionContent2 = document.createElementNS("", "ext:ExtensionContent");
+         ExtensionContent2.appendChild(document.createTextNode("\n"));
+         
+         Element UBLExtension = document.createElementNS("", "ext:UBLExtennsion");
+         elemento.appendChild(UBLExtension);
+         Element ExtensionContent = document.createElementNS("", "ext:ExtensionContent");
+         elemento.appendChild(ExtensionContent);
+         
+         
+//		Element productos = document.createElement("productos");
+//		document.appendChild(productos);
+//
+//		
+//		Element producto = document.createElement("producto");
+//		productos.appendChild(producto);
+//		producto.setAttribute("codigo", "1");
+//		producto.setAttribute("codigo2", "12");
+//		
+//		Element nombre = document.createElement("nombre");
+//		Element nombre2= document.createElement("nombre");
+//		nombre.appendChild(document.createTextNode("Teclado"));
+//		nombre2.appendChild(document.createTextNode("Monitor"));		
+//		producto.appendChild(nombre);
+//		producto.appendChild(nombre2);
+//		
+//		Element producto2 = document.createElement("producto");
+//		productos.appendChild(producto2);
+//		producto2.setAttribute("codigo", "2");
+//		producto2.setAttribute("codigo2", "22");
+//		
+//		Element nombre3 = document.createElement("nombre");
+//		nombre3.appendChild(document.createTextNode("nombre"));
+//		producto2.appendChild(nombre3);
 		
 		
 	
